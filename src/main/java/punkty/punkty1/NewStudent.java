3ee971a3e9132b0d73e3893941e6a48bce39b449
szymon27 <szymonsamsel27@gmail.com>
2023-04-27 18:47:07 +0200

    zad4

--- src/main/java/punkty/punkty1/NewStudent.java
+++ src/main/java/punkty/punkty1/NewStudent.java
@@ -1,11 +1,19 @@
 package punkty.punkty1;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class NewStudent {
     public final String name;
     public final String number;
     public final String grupa;
 
-    public NewStudent(String name, String number, String grupa) {
+    @JsonCreator
+    public NewStudent(
+            @JsonProperty("name") String name,
+            @JsonProperty("number") String number,
+            @JsonProperty("grupa") String grupa
+    ) {
         this.name = name;
         this.number = number;
         this.grupa = grupa;
--- src/main/java/punkty/punkty1/PunktyController.java
+++ src/main/java/punkty/punkty1/PunktyController.java
@@ -5,27 +5,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
+import org.springframework.http.MediaType;
 
 @RestController
 @RequestMapping("/punkty")
 public class PunktyController {
-    private List<String> users;
+    private StudentService service = new StudentService();
 
-    public PunktyController() {
-        this.users = new CopyOnWriteArrayList<>();
-        this.users.addAll(Arrays.asList("Jan Kowalski", "Marek Nowak", "Adam Rak"));
-    }
-    @RequestMapping(value = "/users", method = RequestMethod.GET)
-    public List<String> getUsers() {
-        return this.users;
+    @RequestMapping(value = "/students", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public List<Student> getUsers() {
+        return this.service.getStudents().asJava();
     }
 
-    @RequestMapping(value = "/users", method = RequestMethod.POST)
-    public int addUser(@RequestBody String name) {
-        this.users.add(name);
-        return this.users.size();
+    @RequestMapping(value = "/students", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_UTF8_VALUE, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public Student addUser(@RequestBody NewStudent student) {
+        return this.service.addStudent(student);
     }
 }
